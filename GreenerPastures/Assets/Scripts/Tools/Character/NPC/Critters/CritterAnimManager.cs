a074b0ff8c026ed4350510de2c9b4ca953a60df5
Glenn Storm <GlennStormDesign@gmail.com>
2025-06-04 08:41:53 -0700

    glenn - tool organization revisions; all these blank scripts represent an architecture closely resembling our feature set in greener pastures.

--- GreenerPastures/Assets/Scripts/Tools/Character/NPC/Critters/CritterAnimManager.cs
+++ GreenerPastures/Assets/Scripts/Tools/Character/NPC/Critters/CritterAnimManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class TowerManager : MonoBehaviour
+public class CritterAnimManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- GreenerPastures/Assets/Scripts/Tools/Character/Player/PlayerControlManager.cs
+++ GreenerPastures/Assets/Scripts/Tools/Character/Player/PlayerControlManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class GreenerPlayerControl : MonoBehaviour
+public class PlayerControlManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
