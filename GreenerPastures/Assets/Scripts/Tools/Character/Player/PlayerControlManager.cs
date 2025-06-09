914d734b932102e3a9ca8ac0b6d46ec43675b6dd
Glenn Storm <Glenn.Storm@nyfa.edu>
2025-06-09 12:43:37 -0700

    glenn - fix for #1, player inventory cursor stays.

--- GreenerPastures/Assets/Scripts/Tools/Character/Player/PlayerControlManager.cs
+++ GreenerPastures/Assets/Scripts/Tools/Character/Player/PlayerControlManager.cs
@@ -54,7 +54,7 @@ public class PlayerControlManager : MonoBehaviour
     private SpriteLibraryManager slm;
 
     const float PROXIMITYRANGE = 0.381f;
-    const float INVENTORYSELECTIONTIME = 10f; // was 2f
+    const float INVENTORYSELECTIONTIME = 999f; // was 2f
 
 
     void Start()
