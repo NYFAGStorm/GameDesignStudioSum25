a944f549c642c8691b3b987ef94443be93a86ff1
Glenn Storm <Glenn.Storm@nyfa.edu>
2025-06-29 10:47:44 -0700

    glenn - camera manager script revised for cinematic camera length control with mouse wheel.

--- GreenerPastures/Assets/Scripts/Tools/Character/Player/CameraManager.cs
+++ GreenerPastures/Assets/Scripts/Tools/Character/Player/CameraManager.cs
@@ -21,6 +21,9 @@ public class CameraManager : MonoBehaviour
     public Vector3 cameraTargetPosition;
     public Vector3 cameraTargetRotation;
 
+    public CameraMode modeAfterHold = CameraMode.Follow;
+    public CameraMode modeAfterMove = CameraMode.Hold;
+
     private GameObject playerObject;
     private PlayerControlManager pcm;
 
@@ -29,14 +32,13 @@ public class CameraManager : MonoBehaviour
     private Vector3 savedPostion;
     private Vector3 savedRotation;
 
-    private Vector3 followMoveOffset = new Vector3(0f, 2.5f, -5f);
-    private Vector3 followRotOffset = new Vector3(20f, 0f, 0f);
+    private AnimationCurve easeCurve; // basic ease-in-out curve
 
-    // TODO: utilitze an animation curve for ease-in and ease-out motion
-    // TODO: migrate the calculations done below to world system script
+    private PositionData[] offsetPositions;
+    private PositionData[] offsetRotations;
 
     const float CAMERAPAUSEDURATION = 0.381f;
-    const float CAMERAMOVEDURATION = 1f;
+    const float CAMERAMOVEDURATION = 0.618f;
     const float GLIDEMULTIPLIER = 0.0381f;
     const float PANCRANETARGETVERTICALOFFSET = 0.618f;
     const float MAXPANCRANEDIST = 10f;
@@ -49,10 +51,6 @@ public class CameraManager : MonoBehaviour
         // NOTE: do this here so recovered position and settings from player data can work
         // ensure unparented
         gameObject.transform.parent = null;
-        // start paused
-        cameraPauseTimer = 1f;
-        mode = CameraMode.Hold;
-        SavePosAndRot();
     }
 
     void Start()
@@ -66,11 +64,107 @@ public class CameraManager : MonoBehaviour
                 // ensure unparented
                 gameObject.transform.parent = null;
                 // start paused
-                cameraPauseTimer = 1f;
+                SetDefaultTimers();
                 mode = CameraMode.Hold;
+                modeAfterHold = CameraMode.Follow;
                 SavePosAndRot();
+                // configure cam offsets
+                ConfigureCamOffsets();
+                // config curve
+                easeCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
             }
         }
+    }
+
+    void ConfigureCamOffsets()
+    {
+        offsetPositions = new PositionData[8];
+        offsetRotations = new PositionData[8];
+
+        // DEFAULT
+        offsetPositions[0].x = 0f;
+        offsetPositions[0].y = 2.5f;
+        offsetPositions[0].z = -5f;
+        offsetRotations[0].x = 20f;
+        offsetRotations[0].y = 0f;
+        offsetRotations[0].z = 0f;
+        // FOLLOW
+        offsetPositions[1].x = 0f;
+        offsetPositions[1].y = 2.5f;
+        offsetPositions[1].z = -5f;
+        offsetRotations[1].x = 20f;
+        offsetRotations[1].y = 0f;
+        offsetRotations[1].z = 0f;
+        // HOLD
+        offsetPositions[2].x = 0f;
+        offsetPositions[2].y = 2.5f;
+        offsetPositions[2].z = -5f;
+        offsetRotations[2].x = 20f;
+        offsetRotations[2].y = 0f;
+        offsetRotations[2].z = 0f;
+        // PANFOLLOW
+        offsetPositions[3].x = 0f;
+        offsetPositions[3].y = 2.5f;
+        offsetPositions[3].z = -5f;
+        offsetRotations[3].x = 20f;
+        offsetRotations[3].y = 0f;
+        offsetRotations[3].z = 0f;
+        // CLOSEUP
+        offsetPositions[4].x = 0f;
+        offsetPositions[4].y = 1f;
+        offsetPositions[4].z = -1f;
+        offsetRotations[4].x = 18f;
+        offsetRotations[4].y = 0f;
+        offsetRotations[4].z = 0f;
+        // MEDIUM
+        offsetPositions[5].x = 0f;
+        offsetPositions[5].y = 1.75f;
+        offsetPositions[5].z = -3f;
+        offsetRotations[5].x = 20f;
+        offsetRotations[5].y = 0f;
+        offsetRotations[5].z = 0f;
+        // LONG
+        offsetPositions[6].x = 0f;
+        offsetPositions[6].y = 7.5f;
+        offsetPositions[6].z = -15f;
+        offsetRotations[6].x = 22f;
+        offsetRotations[6].y = 0f;
+        offsetRotations[6].z = 0f;
+        // WORLD
+        offsetPositions[7].x = 0f;
+        offsetPositions[7].y = 20f;
+        offsetPositions[7].z = -45f;
+        offsetRotations[7].x = 30f;
+        offsetRotations[7].y = 0f;
+        offsetRotations[7].z = 0f;
+    }
+
+    Vector3 GetPosOffset( CameraMode mode )
+    {
+        Vector3 retPos = Vector3.zero;
+        PositionData offsetPos = new PositionData();
+
+        offsetPos = offsetPositions[(int)mode];
+
+        retPos.x = offsetPos.x;
+        retPos.y = offsetPos.y;
+        retPos.z = offsetPos.z;
+
+        return retPos;
+    }
+
+    Vector3 GetRotOffset( CameraMode mode )
+    {
+        Vector3 retRot = Vector3.zero;
+        PositionData offsetRot = new PositionData();
+
+        offsetRot = offsetRotations[(int)mode];
+
+        retRot.x = offsetRot.x;
+        retRot.y = offsetRot.y;
+        retRot.z = offsetRot.z;
+
+        return retRot;
     }
 
     /// <summary>
@@ -88,9 +182,9 @@ public class CameraManager : MonoBehaviour
     /// </summary>
     public void SetCameraFollowMode()
     {
+        mode = CameraMode.Follow;
         GetFollowTarget();
         gameObject.transform.eulerAngles = savedRotation;
-        mode = CameraMode.Follow;
     }
 
     /// <summary>
@@ -118,8 +212,9 @@ public class CameraManager : MonoBehaviour
 
     void GetFollowTarget()
     {
-        cameraTargetPosition = playerObject.transform.position + followMoveOffset;
-        cameraTargetRotation = followRotOffset;
+        cameraTargetPosition = playerObject.transform.position;
+        cameraTargetPosition += GetPosOffset(mode);
+        cameraTargetRotation = GetRotOffset(mode);
     }
 
     void GetPanTarget()
@@ -141,25 +236,13 @@ public class CameraManager : MonoBehaviour
         cameraTargetRotation = camTrans.eulerAngles;
     }
 
-    void SetTimers()
+    void SetDefaultTimers()
     {
         cameraPauseTimer = CAMERAPAUSEDURATION;
         cameraMoveTimer = CAMERAMOVEDURATION;
     }
 
-    void PerformMove( Vector3 posOffset, Vector3 rotOffset )
-    {
-        Vector3 pos = gameObject.transform.position;
-        Vector3 rot = gameObject.transform.eulerAngles;
-
-        pos += (cameraTargetPosition - pos) * GLIDEMULTIPLIER;
-        rot += (cameraTargetRotation - rot) * GLIDEMULTIPLIER;
-
-        gameObject.transform.position = pos;
-        gameObject.transform.eulerAngles = rot;
-    }
-
-    void PerformPan( Vector3 posOffset, Vector3 rotOffset )
+    void PerformMove()
     {
         Vector3 pos = gameObject.transform.position;
         Vector3 rot = gameObject.transform.eulerAngles;
@@ -176,11 +259,43 @@ public class CameraManager : MonoBehaviour
         if (pcm == null)
             return;
 
+        // detect player cam controls
+        if ((mode == CameraMode.Follow || mode > CameraMode.PanFollow) && 
+            cameraPauseTimer == 0f && cameraMoveTimer == 0f)
+        {
+            // TODO: gamepad controls (dpad up and down)
+            int camModeChange = 0;
+            camModeChange += (int)Input.mouseScrollDelta.y;
+            if (camModeChange != 0)
+            {
+                if (mode == CameraMode.Follow)
+                    modeAfterHold = CameraMode.CloseUp;
+                else
+                    modeAfterHold -= camModeChange;
+                if (modeAfterHold == CameraMode.PanFollow)
+                    modeAfterHold = CameraMode.Follow;
+                if (modeAfterHold > CameraMode.World)
+                    modeAfterHold = CameraMode.World;
+                else
+                {
+                    SavePosAndRot();
+                    // use modeAfterHold for target acquisition
+                    mode = modeAfterHold;
+                    GetFollowTarget();
+                    SetDefaultTimers();
+                    mode = CameraMode.Hold;
+                    modeAfterMove = modeAfterHold; // stay there
+                }
+            }
+        }
+
         if (mode == CameraMode.Follow)
         {
             GetFollowTarget();
-            // move camera
-            PerformMove( followMoveOffset, followRotOffset );
+            // follow move camera
+            PerformMove();
+            // reset move timer
+            cameraMoveTimer = 0f;
             return;
         }
 
@@ -193,10 +308,8 @@ public class CameraManager : MonoBehaviour
                 if (cameraPauseTimer < 0f)
                 {
                     cameraPauseTimer = 0f;
-                    // handle end of pause
-                    
-                    // temp
-                    mode = CameraMode.Follow;
+                    // handle mode after hold
+                    mode = modeAfterHold;
                     return;
                 }
                 else
@@ -207,30 +320,75 @@ public class CameraManager : MonoBehaviour
         if ( mode == CameraMode.PanFollow )
         {
             GetPanTarget();
-            // pan camera
-            PerformPan(followMoveOffset, followRotOffset);
+            // pan move camera
+            PerformMove();
             return;
         }
 
-        // NOTE: for cinematic camera use (not default follow player mode)
+        // CINEMATIC CAMERA MOVES
 
         // run move timer
         if (cameraMoveTimer > 0f)
         {
             cameraMoveTimer -= Time.deltaTime;
             if (cameraMoveTimer < 0f)
-            {
                 cameraMoveTimer = 0f;
-            }
         }
-        float progress = ((CAMERAMOVEDURATION - cameraMoveTimer)/CAMERAMOVEDURATION);
+        else
+        {
+            // at end of cinematic moves, allow follow behavior
+            // (close up and medium only)
+            if (mode == CameraMode.CloseUp || mode == CameraMode.Medium)
+            {
+                GetFollowTarget();
+                // follow move camera
+                PerformMove();
+            }
+            return;
+        }
+
+        // smooth progress to target
+        float progress = ((CAMERAMOVEDURATION - cameraMoveTimer) / CAMERAMOVEDURATION);
+        if (easeCurve != null)
+            progress = easeCurve.Evaluate(progress);
 
         // move camera
-        gameObject.transform.position = Vector3.Lerp(savedPostion,cameraTargetPosition,progress);
+        gameObject.transform.position = Vector3.Lerp(savedPostion, cameraTargetPosition, progress);
         gameObject.transform.eulerAngles = Vector3.Lerp(savedRotation, cameraTargetRotation, progress);
 
-        // reset timers
+        // progress done
         if (progress >= 1f)
-            SetTimers();
+        {
+            SavePosAndRot();
+            // handle mode after move
+            mode = modeAfterMove;
+        }
     }
+
+    /*
+    void OnGUI()
+    {
+        Rect r = new Rect();
+        float w = Screen.width;
+        float h = Screen.height;
+
+        r.x = 0.1f * w;
+        r.y = 0.3f * h;
+        r.width = 0.2f * w;
+        r.height = 0.2f * h;
+        GUIStyle g = new GUIStyle(GUI.skin.label);
+        g.fontSize = Mathf.RoundToInt(16f * (w / 1024f));
+        g.wordWrap = true;
+        string s = "camera mode: ";
+        s += mode.ToString() + "\n";
+        s += "pause: " + cameraPauseTimer + "\n";
+        s += "move: " + cameraMoveTimer + "\n";
+        Color c = Color.white;
+        c.r = 0.381f;
+        c.g = 0.618f;
+
+        GUI.color = c;
+        GUI.Label(r, s, g);
+    }
+    */
 }
