d9871c11cae3626a8e3ea8a6c16630f04a16bca2
Glenn Storm <Glenn.Storm@nyfa.edu>
2025-07-02 19:15:16 -0700

    glenn - player system script revised for helper functions GetPlayerSkinColor and GetPlayerColor.

--- GreenerPastures/Assets/Scripts/Systems/PlayerSystem.cs
+++ GreenerPastures/Assets/Scripts/Systems/PlayerSystem.cs
@@ -30,4 +30,103 @@ public static class PlayerSystem
 
         return retPlayer;
     }
+
+    public static UnityEngine.Color GetPlayerSkinColor( PlayerSkinColor tone )
+    {
+        UnityEngine.Color retColor = new UnityEngine.Color();
+
+        switch (tone)
+        {
+            case PlayerSkinColor.Default:
+                // we should never be here
+                break;
+            case PlayerSkinColor.ToneA:
+                retColor.r = 0.25f;
+                retColor.g = 0.2f;
+                retColor.b = 0.125f;
+                break;
+            case PlayerSkinColor.ToneB:
+                retColor.r = 0.3f;
+                retColor.g = 0.25f;
+                retColor.b = 0.175f;
+                break;
+            case PlayerSkinColor.ToneC:
+                retColor.r = 0.45f;
+                retColor.g = 0.381f;
+                retColor.b = 0.27f;
+                break;
+            case PlayerSkinColor.ToneD:
+                retColor.r = 0.55f;
+                retColor.g = 0.45f;
+                retColor.b = 0.333f;
+                break;
+            case PlayerSkinColor.ToneE:
+                retColor.r = 0.67f;
+                retColor.g = 0.575f;
+                retColor.b = 0.45f;
+                break;
+            case PlayerSkinColor.ToneF:
+                retColor.r = 0.7f;
+                retColor.g = 0.6f;
+                retColor.b = 0.5f;
+                break;
+            case PlayerSkinColor.ToneG:
+                retColor.r = 0.75f;
+                retColor.g = 0.7f;
+                retColor.b = 0.618f;
+                break;
+        }
+        retColor.a = 1f;
+
+        return retColor;
+    }
+
+    public static UnityEngine.Color GetPlayerColor( PlayerColor tone )
+    {
+        UnityEngine.Color retColor = new UnityEngine.Color();
+
+        switch (tone)
+        {
+            case PlayerColor.Default:
+                // we should never be here
+                break;
+            case PlayerColor.ColorA:
+                retColor.r = 0.8f;
+                retColor.g = 0.8f;
+                retColor.b = 0.8f;
+                break;
+            case PlayerColor.ColorB:
+                retColor.r = 0.618f;
+                retColor.g = 0.618f;
+                retColor.b = 0.618f;
+                break;
+            case PlayerColor.ColorC:
+                retColor.r = 0.8f;
+                retColor.g = 0.3f;
+                retColor.b = 0.3f;
+                break;
+            case PlayerColor.ColorD:
+                retColor.r = 0.55f;
+                retColor.g = 0.5f;
+                retColor.b = 0.8f;
+                break;
+            case PlayerColor.ColorE:
+                retColor.r = 0.3f;
+                retColor.g = 0.7f;
+                retColor.b = 0.8f;
+                break;
+            case PlayerColor.ColorF:
+                retColor.r = 0.125f;
+                retColor.g = 0.618f;
+                retColor.b = 0.3f;
+                break;
+            case PlayerColor.ColorG:
+                retColor.r = 0.8f;
+                retColor.g = 0.618f;
+                retColor.b = 0.381f;
+                break;
+        }
+
+        return retColor;
+    }
 }
