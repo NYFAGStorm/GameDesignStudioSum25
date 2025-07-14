9308e1135d328512fc7425757db1f2102068859d
Glenn Storm <Glenn.Storm@nyfa.edu>
2025-07-14 07:14:36 -0700

    glenn - moved craft shape creator tool to 'art creation' tools, craft shape data tool revised for 3x3 ingredient shape bounds and applying both item type and plant type.

--- GreenerPastures/Assets/Scripts/Tools/Art Creation/CraftShapeData.cs
+++ GreenerPastures/Assets/Scripts/Tools/Art Creation/CraftShapeData.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 
 // Author: Gustavo Rojas Flores
-// Allows easy editing of crafting shape data for crafting spells with plants
+// Allows easy editing of crafting shape data for crafting spells with ingredients
 
 [CreateAssetMenu(fileName = "Crafting Shape", menuName = "Greener Pastures/Crafting Shape", order = 1)]
 public class CraftShape : ScriptableObject
 {
+    public ItemType itemType;
     public PlantType plantType;
     public Sprite craftingTileImage;
-    public bool[] craftingGridShape = new bool[25];
+    public bool[] craftingGridShape = new bool[9];
 }
 
 // Custom inspector
@@ -25,21 +26,23 @@ public class CraftingShapeEditor : Editor
             serializedObject.Update();
 
             // Create serialized properties
+            SerializedProperty iType = serializedObject.FindProperty("itemType");
             SerializedProperty pType = serializedObject.FindProperty("plantType");
             SerializedProperty tileImage = serializedObject.FindProperty("craftingTileImage");
             SerializedProperty boolGrid = serializedObject.FindProperty("craftingGridShape");
 
             // Draw first two properties
+            EditorGUILayout.PropertyField(iType);
             EditorGUILayout.PropertyField(pType);
             EditorGUILayout.ObjectField(tileImage);
             
             // Create boolean grid with header
-            EditorGUILayout.LabelField("Crafting Grid Shape", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Magic Crafting Ingredient Shape", EditorStyles.boldLabel);
             
-            for (int y = 0; y < 5; y++)
+            for (int y = 0; y < 3; y++)
             {
                 EditorGUILayout.BeginHorizontal();
-                for (int x = 0; x < 5; x++)
+                for (int x = 0; x < 3; x++)
                 {
                     SerializedProperty craftTile = boolGrid.GetArrayElementAtIndex(y * 5 + x);
                     craftTile.boolValue = GUILayout.Toggle(craftTile.boolValue, GUIContent.none, GUILayout.Width(13));
