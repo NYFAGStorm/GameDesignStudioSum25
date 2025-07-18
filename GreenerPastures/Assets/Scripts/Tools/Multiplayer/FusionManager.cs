612fe7f45ce1381a0934c40e43f69aa1b4ef591e
amoam-vr <143439516+amoam-vr@users.noreply.github.com>
2025-07-17 21:53:51 -0700

    added fusion manager script
    Q: Where should I connect StartMultiplayerGame() and within which script?

---
+++ GreenerPastures/Assets/Scripts/Tools/Multiplayer/FusionManager.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Fusion;
+using Fusion.Sockets;
+
+public class FusionManager : MonoBehaviour, INetworkRunnerCallbacks
+{
+    // Author: Gustavo Rojas Flores
+    // Handles all things connected to network management
+    
+    // Q: Where should I connect StartMultiplayerGame() and within which script?
+
+    private NetworkRunner Runner;
+
+    // Use GameMode.Host and GameMode.Client to determine join type
+    async void StartMultiplayerGame(GameMode mode, string code)
+    {
+        Runner = gameObject.AddComponent<NetworkRunner>();
+        Runner.ProvideInput = true;
+
+        var scene = SceneRef.FromIndex(SceneManager.GetActiveScene().buildIndex);
+        var sceneInfo = new NetworkSceneInfo();
+        if (scene.IsValid) sceneInfo.AddSceneRef(scene, LoadSceneMode.Additive);
+
+        await Runner.StartGame(new StartGameArgs()
+        {
+            GameMode = mode,
+            SessionName = code,
+            Scene = scene,
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        }
+        );
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        if (Runner.IsServer)
+        {
+            //players[plrCount] = player;
+            //plrCount++;
+        }
+    }
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        if (Runner.IsServer)
+        {
+            //players[plrCount] = PlayerRef.None;
+            //plrCount--;
+        }
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        // Receive client-sided input here
+
+        //input.Set(playerInput);
+    }
+
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+
+    }
+
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason exit) { }
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+}
