be0fe574a53966521a403efbf59a7a6ca4c86c00
Glenn Storm <Glenn.Storm@nyfa.edu>
2025-07-31 15:02:45 -0700

    glenn - #66, arcana skill manager script revised to display skill tree and allow keyboard navigation of tree nodes to explore skill descriptions.

--- GreenerPastures/Assets/Scripts/Tools/Game/ArcanaSkilManager.cs
+++ GreenerPastures/Assets/Scripts/Tools/Game/ArcanaSkilManager.cs
@@ -13,8 +13,20 @@ public class ArcanaSkilManager : MonoBehaviour
 
     public PlayerControlManager currentPlayer;
     public bool skillPurchasing;
+    public int currentSkillNode;
 
     public SkillTree skillTree;
+
+    public struct SkillNode
+    {
+        public string name;
+        public int nodeDepth;
+        public int parent;
+        public Vector2 position;
+        public bool acquired;
+        public bool available;
+    }
+    private SkillNode[] nodes;
 
     private bool displaySkillTree;
 
@@ -27,6 +39,7 @@ public class ArcanaSkilManager : MonoBehaviour
         {
             // build test tree
             InitializeSkillTree();
+            InitializeNodes();
         }
     }
 
@@ -56,7 +69,7 @@ public class ArcanaSkilManager : MonoBehaviour
         skill = SkillSystem.InitializeSkill("Friends of the Chicken",
             "Your bet attracts more wagers to the gold dish.", PlayerEffect.SkillFriendsChicken, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "root");
-        skill = SkillSystem.InitializeSkill("Friends of the Marchant",
+        skill = SkillSystem.InitializeSkill("Friends of the Merchant",
             "Selling at the market every 25 times earns you a 100% off coupon for a single item purchase.", PlayerEffect.SkillFriendsMerchant, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Friends of the Chicken");
         skill = SkillSystem.InitializeSkill("Friends of the Gold Fairy",
@@ -64,7 +77,7 @@ public class ArcanaSkilManager : MonoBehaviour
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Friends of the Chicken");
         skill = SkillSystem.InitializeSkill("Friends of the Salesman",
             "Your salesman purchases on island upgrades are at a 25% discount.", PlayerEffect.SkillFriendsSalesman, 1);
-        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Friends of the Marchant");
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Friends of the Merchant");
         skill = SkillSystem.InitializeSkill("TBD Gold",
             "Pretty cool gold skill buff.", PlayerEffect.SkillGoldTBD, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Friends of the Gold Fairy");
@@ -104,6 +117,51 @@ public class ArcanaSkilManager : MonoBehaviour
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "So Crafty");
     }
 
+    void InitializeNodes()
+    {
+        nodes = new SkillNode[skillTree.skills.Length];
+        for (int i = 0; i < skillTree.skills.Length; i++)
+        {
+            nodes[i].name = skillTree.skills[i].name;
+            nodes[i].nodeDepth = SkillSystem.GetSkillNodeDepth(skillTree, nodes[i].name);
+            nodes[i].parent = skillTree.skills[i].parent;
+            nodes[i].position = Vector2.zero;
+            nodes[i].position.x = 0.1f + ((nodes[i].nodeDepth-1) * .2875f);
+            nodes[i].position.y = .1625f;
+            switch (nodes[i].nodeDepth - 1)
+            {
+                case 0:
+                    nodes[i].position.y += .1125f;
+                    nodes[i].position.y += (i / 5) * .15f;
+                    break;
+                case 1:
+                    if (i % 5 == 1 || i % 5 == 3)
+                        nodes[i].position.y += .075f;
+                    else if (i % 5 == 2 || i % 5 == 4)
+                        nodes[i].position.y += .15f;
+                    nodes[i].position.y += (i / 5) * .15f;
+                    break;
+                case 2:
+                    if (i % 5 == 1 || i % 5 == 3)
+                        nodes[i].position.y += .075f;
+                    else if (i % 5 == 2 || i % 5 == 4)
+                        nodes[i].position.y += .15f;
+                    nodes[i].position.y += (i / 5) * .15f;
+                    break;
+            }
+            if (currentPlayer != null)
+                nodes[i].acquired = PlayerSystem.PlayerHasEffect(currentPlayer.playerData, skillTree.skills[i].playerSkill);
+        }
+        // determine available
+        for (int i = 0; i < skillTree.skills.Length; i++)
+        {
+            if (nodes[i].parent == -1)
+                nodes[i].available = true;
+            else
+                nodes[i].available = nodes[nodes[i].parent].acquired;
+        }
+    }
+
     void Update()
     {
         // test
@@ -116,10 +174,65 @@ public class ArcanaSkilManager : MonoBehaviour
         // detect skill purchasing
         if (skillPurchasing && currentPlayer != null)
         {
+            InitializeNodes();
             displaySkillTree = true;
         }
         else if (displaySkillTree)
             displaySkillTree = false;
+
+        if (!displaySkillTree)
+            return;
+
+        // keybvoard navigation of skill tree
+        // WASD
+        int move = -1;
+        if (Input.GetKeyDown(KeyCode.W))
+            move = 0;
+        if (Input.GetKeyDown(KeyCode.S))
+            move = 1;
+        if (Input.GetKeyDown(KeyCode.A))
+            move = 2;
+        if (Input.GetKeyDown(KeyCode.D))
+            move = 3;
+        if (move < 0)
+            return;
+
+        if (nodes[currentSkillNode].parent == -1)
+        {
+            if (move == 0)
+                currentSkillNode -= 5;
+            if (currentSkillNode < 0)
+                currentSkillNode = 0;
+            if (move == 1)
+                currentSkillNode += 5;
+            if (currentSkillNode > 15)
+                currentSkillNode = 15;
+            if (move == 3 && nodes[currentSkillNode].acquired)
+                currentSkillNode += 1;
+            return;
+        }
+        if (nodes[currentSkillNode].nodeDepth == 2)
+        {
+            if (move == 0 && currentSkillNode % 5 == 2)
+                currentSkillNode -= 1;
+            if (move == 1 && currentSkillNode % 5 == 1)
+                currentSkillNode += 1;
+            if (move == 2)
+            {
+                if (currentSkillNode % 5 == 1)
+                    currentSkillNode -= 1;
+                else
+                    currentSkillNode -= 2;
+            }
+            if (move == 3 && nodes[currentSkillNode].acquired)
+                currentSkillNode += 2;
+            return;
+        }
+        if (nodes[currentSkillNode].nodeDepth == 3)
+        {
+            if (move == 2)
+                currentSkillNode -= 2;
+        }
     }
 
     void OnGUI()
@@ -143,7 +256,119 @@ public class ArcanaSkilManager : MonoBehaviour
         string s = "ARCANA SKILL TREE";
         GUI.Box(r, s, g);
 
-        // tree nodes
+        Color c = Color.white;
+
+        // skill tree
+        //  4 root nodes
+        //  4 nodes per root, two branches
+        //  3 columns, 4 rows
+        //  box + label + checkbox per node
+        for (int i = 0; i < skillTree.skills.Length; i++)
+        {
+            r.x = nodes[i].position.x * w;
+            r.y = nodes[i].position.y * h;
+            r.width = 0.25f * w;
+            r.height = 0.05f * h;
+
+            g = new GUIStyle(GUI.skin.box);
+            GUI.Box(r, "", g);
+
+            r.x += 0.01f * w;
+            r.y += 0.00125f * h;
+            r.width -= 0.005f * w;
+            r.height -= 0.002f * h;
+
+            g = new GUIStyle(GUI.skin.label);
+            g.alignment = TextAnchor.MiddleCenter;
+            g.fontSize = Mathf.RoundToInt(18 * (w / 1024f));
+            g.fontStyle = FontStyle.Normal;
+            if (currentSkillNode == i)
+                g.fontStyle = FontStyle.Bold;
+            c = Color.white;
+            c *= 0.618f;
+            g.normal.textColor = c;
+            g.hover.textColor = c;
+            g.active.textColor = c;
+            if (nodes[i].available)
+            {
+                c = Color.white;
+                c *= 0.9f;
+                g.normal.textColor = c;
+                g.hover.textColor = c;
+                g.active.textColor = c;
+            }
+            else if (nodes[i].acquired)
+            {
+                g.normal.textColor = Color.white;
+                g.hover.textColor = Color.white;
+                g.active.textColor = Color.white;
+            }
+            if (currentSkillNode == i)
+            {
+                g.normal.textColor = Color.yellow;
+                g.hover.textColor = Color.yellow;
+                g.active.textColor = Color.yellow;
+            }
+            s = nodes[i].name;
+            GUI.Label(r, s, g);
+
+            // check boxes
+            r.x -= 0.035f * w;
+            r.y += 0.005f * h;
+            r.width = 0.04f * h;
+            r.height = 0.04f * h; // square
+            g = new GUIStyle(GUI.skin.box);
+            GUI.Box(r, "", g);
+            // checks
+            g = new GUIStyle(GUI.skin.label);
+            g.alignment = TextAnchor.MiddleCenter;
+            g.fontSize = Mathf.RoundToInt(20 * (w / 1024f));
+            g.fontStyle = FontStyle.Bold;
+            g.normal.textColor = Color.white;
+            g.hover.textColor = Color.white;
+            g.active.textColor = Color.white;
+            GUI.Box(r, nodes[i].acquired ? "X" : "", g);
+        }
+
+        // skill description display
+        r.x = 0.05f * w;
+        r.y = 0.8325f * h;
+        r.width = 0.9f * w;
+        r.height = 0.05f * h;
+        g = new GUIStyle(GUI.skin.label);
+        g.alignment = TextAnchor.MiddleCenter;
+        g.fontSize = Mathf.RoundToInt(18 * (w / 1024f));
+        g.fontStyle = FontStyle.Italic;
+        g.normal.textColor = Color.yellow;
+        g.hover.textColor = Color.yellow;
+        g.active.textColor = Color.yellow;
+        s = skillTree.skills[currentSkillNode].description;
+        GUI.Label(r, s, g);
+
+        // arcana display
+        r.x = 0.05f * w;
+        r.y = 0.875f * h;
+        r.width = 0.15f * w;
+        r.height = 0.1f * h;
+        g = new GUIStyle(GUI.skin.label);
+        g.alignment = TextAnchor.MiddleCenter;
+        g.fontSize = Mathf.RoundToInt(20 * (w / 1024f));
+        g.fontStyle = FontStyle.Bold;
+        s = "";
+        if (currentPlayer != null)
+            s = "ARCANA: " + currentPlayer.playerData.arcana;
+        r.x += 0.001f * w;
+        r.y += 0.001f * w;
+        g.normal.textColor = Color.black;
+        g.hover.textColor = Color.black;
+        g.active.textColor = Color.black;
+        GUI.Label(r, s, g);
+        r.x -= 0.002f * w;
+        r.y -= 0.002f * w;
+        g.normal.textColor = Color.yellow;
+        g.hover.textColor = Color.yellow;
+        g.active.textColor = Color.yellow;
+        GUI.Label(r, s, g);
 
         // cancel button
         r.x = 0.4f * w;
