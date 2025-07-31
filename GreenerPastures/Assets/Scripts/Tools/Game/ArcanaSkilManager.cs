e88d2cdc4e0aa7683918fccb1d9205dcf4b233dd
Glenn Storm <Glenn.Storm@nyfa.edu>
2025-07-31 12:10:18 -0700

    glenn - #66, arcana skill manager revised to initialize skill tree per details on spreadsheet as of noon 7-31, player data script revised to include skill-based player effects.

--- GreenerPastures/Assets/Scripts/Tools/Game/ArcanaSkilManager.cs
+++ GreenerPastures/Assets/Scripts/Tools/Game/ArcanaSkilManager.cs
@@ -25,29 +25,75 @@ public class ArcanaSkilManager : MonoBehaviour
     void InitializeSkillTree()
     {
         skillTree = SkillSystem.InitializeSkillTree();
-        
-        // gold branch
-        SkillData skill = SkillSystem.InitializeSkill("Friends Of The Chicken", 
-            "Your bet attracts more wagers in the gold dish.", PlayerEffect.EdenLetterOne, 1);
-        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "root");
-        skill = SkillSystem.InitializeSkill("Friends Of The Merchant", 
-            "Every 25 purchases at the market earn you a 100% off coupon.", PlayerEffect.EdenLetterTwo, 1);
-        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Friends Of The Chicken");
-        skill = SkillSystem.InitializeSkill("Friends Of The Salesman", 
-            "The traveling salesman gives you a 25% discount on all his wares.", PlayerEffect.EdenLetterThree, 1);
-        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Friends Of The Merchant");
+        SkillData skill = null;
 
-        // magic branch
-        skill = SkillSystem.InitializeSkill("Cool Cat", 
-            "All spell book cooldowns are half the duration.", PlayerEffect.EdenLetterFour, 1);
+        // farming skill branch
+        skill = SkillSystem.InitializeSkill("Cover Crop",
+            "Harvesting non-refruiting plants, automatically uproots stalk and replants if you hold seed.", PlayerEffect.SkillCoverCrop, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "root");
-        skill = SkillSystem.InitializeSkill("So Crafty", 
-            "All crafted spells yeild two charges in your spell book.", PlayerEffect.EdenLetterFive, 1);
+        skill = SkillSystem.InitializeSkill("Focus Flow",
+            "While performing the same action repeatedly on multiple plots, your work goes twice as fast.", PlayerEffect.SkillFocusFlow, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Cover Crop");
+        skill = SkillSystem.InitializeSkill("Seed Fairy Magnet",
+            "Each morning has a chance of a seed fairy visit, and she drops one unique plant seed.", PlayerEffect.SkillSeedFairy, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Cover Crop");
+        skill = SkillSystem.InitializeSkill("TBD Farming",
+            "Pretty cool farming skill buff.", PlayerEffect.SkillFarmingTBD, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Focus Flow");
+        skill = SkillSystem.InitializeSkill("Dark Biomage",
+            "Dark plant seeds you grow at night do very well even during the New Moon and low moonlight.", PlayerEffect.SkillDarkBiomage, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Seed Fairy Magnet");
+
+        // gold skill branch
+        skill = SkillSystem.InitializeSkill("Friends of the Chicken",
+            "Your bet attracts more wagers to the gold dish.", PlayerEffect.SkillFriendsChicken, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "root");
+        skill = SkillSystem.InitializeSkill("Friends of the Marchant",
+            "Selling at the market every 25 times earns you a 100% off coupon for a single item purchase.", PlayerEffect.SkillFriendsMerchant, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Friends of the Chicken");
+        skill = SkillSystem.InitializeSkill("Friends of the Gold Fairy",
+            "The Gold Fairy visits your farm most nights.", PlayerEffect.SkillFriendsGoldFairy, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Friends of the Chicken");
+        skill = SkillSystem.InitializeSkill("Friends of the Salesman",
+            "Your salesman purchases on island upgrades are at a 25% discount.", PlayerEffect.SkillFriendsSalesman, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Friends of the Marchant");
+        skill = SkillSystem.InitializeSkill("TBD Gold",
+            "Pretty cool gold skill buff.", PlayerEffect.SkillGoldTBD, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Friends of the Gold Fairy");
+
+        // active magic skill branch
+        skill = SkillSystem.InitializeSkill("Waste Management",
+            "Covert one fertilizer to a random seed.", PlayerEffect.SkillWasteManagement, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "root");
+        skill = SkillSystem.InitializeSkill("Clean Up",
+            "All loose plant-based items within area are deposited in the compost bin.", PlayerEffect.SkillCleanUp, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Waste Management");
+        skill = SkillSystem.InitializeSkill("TBD Active Magic",
+            "Pretty cool active magic skill buff.", PlayerEffect.SkillActiveMagicTBD, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Waste Management");
+        skill = SkillSystem.InitializeSkill("Cash In",
+            "All loose items within area are sold at the market, turning them into a gold pouch.", PlayerEffect.SkillCashIn, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Clean Up");
+        skill = SkillSystem.InitializeSkill("Take Me Home",
+            "Immediately teleports you back home from anywhere.", PlayerEffect.SkillTakeMeHome, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "TBD Active Magic");
+
+        // passive magic skill branch
+        skill = SkillSystem.InitializeSkill("Cool Cat",
+            "All spell cooldowns go twice as fast.", PlayerEffect.SkillCoolCat, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "root");
+        skill = SkillSystem.InitializeSkill("Mystic Forager",
+            "You are likely to discover wild plots with any plant growing in them.", PlayerEffect.SkillMysticForager, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Cool Cat");
-        skill = SkillSystem.InitializeSkill("Archmage", 
-            "All spells have an area of effect twice as large.", PlayerEffect.EdenLetterSix, 1);
+        skill = SkillSystem.InitializeSkill("So Crafty",
+            "All crafted spells yield two charges in your spell book.", PlayerEffect.SkillSoCrafty, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Cool Cat");
+        skill = SkillSystem.InitializeSkill("TBD Passive Magic",
+            "Pretty cool passive magic skill buff.", PlayerEffect.SkillPassiveMagicTBD, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Mystic Forager");
+        skill = SkillSystem.InitializeSkill("Archmage",
+            "All areas of effect on spells are twice as big.", PlayerEffect.SkillArchmage, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "So Crafty");
-
     }
 
     void Update()
