0ce6394dea9a2b8c6f7ba19caf00e5d4c3882983
Glenn Storm <Glenn.Storm@nyfa.edu>
2025-08-01 10:03:22 -0700

    glenn - #66, player data script revised for new skill names, arcana skill manager revised for restructured skill tree, save load manager script revised for version number increment (data structure change).

--- GreenerPastures/Assets/Scripts/Tools/Game/ArcanaSkilManager.cs
+++ GreenerPastures/Assets/Scripts/Tools/Game/ArcanaSkilManager.cs
@@ -75,15 +75,15 @@ public class ArcanaSkilManager : MonoBehaviour
         skill = SkillSystem.InitializeSkill("Focus Flow",
             "While performing the same action repeatedly on multiple plots, your work goes twice as fast.", PlayerEffect.SkillFocusFlow, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Cover Crop");
-        skill = SkillSystem.InitializeSkill("Seed Fairy Magnet",
-            "Each morning has a chance of a seed fairy visit, and she drops one unique plant seed.", PlayerEffect.SkillSeedFairy, 1);
-        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Cover Crop");
-        skill = SkillSystem.InitializeSkill("TBD Farming",
-            "Pretty cool farming skill buff.", PlayerEffect.SkillFarmingTBD, 1);
-        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Focus Flow");
         skill = SkillSystem.InitializeSkill("Dark Biomage",
             "Dark plant seeds you grow at night do very well even during the New Moon and low moonlight.", PlayerEffect.SkillDarkBiomage, 1);
-        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Seed Fairy Magnet");
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Cover Crop");
+        skill = SkillSystem.InitializeSkill("Seed Fairy Magnet",
+            "Each morning has a chance of a seed fairy visit, and she drops one unique plant seed.", PlayerEffect.SkillSeedFairy, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Focus Flow");
+        skill = SkillSystem.InitializeSkill("Plant Doctor",
+            "No matter the season, all your plants have 100% seasonal vitality.", PlayerEffect.SkillPlantDoctor, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Dark Biomage");
 
         // gold skill branch
         skill = SkillSystem.InitializeSkill("Friends of the Chicken",
@@ -98,8 +98,8 @@ public class ArcanaSkilManager : MonoBehaviour
         skill = SkillSystem.InitializeSkill("Friends of the Salesman",
             "Your salesman purchases on island upgrades are at a 25% discount.", PlayerEffect.SkillFriendsSalesman, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Friends of the Merchant");
-        skill = SkillSystem.InitializeSkill("TBD Gold",
-            "Pretty cool gold skill buff.", PlayerEffect.SkillGoldTBD, 1);
+        skill = SkillSystem.InitializeSkill("Midas Biomancer",
+            "Harvesting each plant has a 5% chance of dropping a gold sack instead, worth twice the fruit value.", PlayerEffect.SkillMidasBiomancer, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Friends of the Gold Fairy");
 
         // active magic skill branch
@@ -109,32 +109,32 @@ public class ArcanaSkilManager : MonoBehaviour
         skill = SkillSystem.InitializeSkill("Clean Up",
             "All loose plant-based items within area are deposited in the compost bin.", PlayerEffect.SkillCleanUp, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Waste Management");
-        skill = SkillSystem.InitializeSkill("TBD Active Magic",
-            "Pretty cool active magic skill buff.", PlayerEffect.SkillActiveMagicTBD, 1);
+        skill = SkillSystem.InitializeSkill("Lighten Up",
+            "You summon a personal light overhead for 3 hours.", PlayerEffect.SkillLightenUp, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Waste Management");
         skill = SkillSystem.InitializeSkill("Cash In",
             "All loose items within area are sold at the market, turning them into a gold pouch.", PlayerEffect.SkillCashIn, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Clean Up");
         skill = SkillSystem.InitializeSkill("Take Me Home",
             "Immediately teleports you back home from anywhere.", PlayerEffect.SkillTakeMeHome, 1);
-        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "TBD Active Magic");
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Lighten Up");
 
         // passive magic skill branch
-        skill = SkillSystem.InitializeSkill("Cool Cat",
-            "All spell cooldowns go twice as fast.", PlayerEffect.SkillCoolCat, 1);
+        skill = SkillSystem.InitializeSkill("Grocery List",
+            "Spell ingredients from last highlighted Grimoire recipe are listed on the HUD.", PlayerEffect.SkillGroceryList, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "root");
-        skill = SkillSystem.InitializeSkill("Mystic Forager",
-            "You are likely to discover wild plots with any plant growing in them.", PlayerEffect.SkillMysticForager, 1);
-        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Cool Cat");
         skill = SkillSystem.InitializeSkill("So Crafty",
             "All crafted spells yield two charges in your spell book.", PlayerEffect.SkillSoCrafty, 1);
-        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Cool Cat");
-        skill = SkillSystem.InitializeSkill("TBD Passive Magic",
-            "Pretty cool passive magic skill buff.", PlayerEffect.SkillPassiveMagicTBD, 1);
-        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Mystic Forager");
-        skill = SkillSystem.InitializeSkill("Archmage",
-            "All areas of effect on spells are twice as big.", PlayerEffect.SkillArchmage, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Grocery List");
+        skill = SkillSystem.InitializeSkill("Cool Cat",
+            "All spell book cooldowns go twice as fast.", PlayerEffect.SkillCoolCat, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Grocery List");
+        skill = SkillSystem.InitializeSkill("Mystic Forager",
+            "Some mornings, you can sense wild plots have appeared with any seedling growing.", PlayerEffect.SkillMysticForager, 1);
         skillTree = SkillSystem.AddTreeNode(skillTree, skill, "So Crafty");
+        skill = SkillSystem.InitializeSkill("Archmage",
+            "All spell cast areas of effect are twice as big.", PlayerEffect.SkillArchmage, 1);
+        skillTree = SkillSystem.AddTreeNode(skillTree, skill, "Cool Cat");
     }
 
     void InitializeNodes()
--- GreenerPastures/Assets/Scripts/Tools/Game/SaveLoadManager.cs
+++ GreenerPastures/Assets/Scripts/Tools/Game/SaveLoadManager.cs
@@ -39,7 +39,7 @@ public class SaveLoadManager : MonoBehaviour
     const string GAMESPATH = "/Games/";
     const string GAMEFILEPREFIX = "GreenerGame-";
     const string GAMEFILESUFFIX = ".dat";
-    const string VERSIONNUMBERSTRING = "07.31.0453.f";
+    const string VERSIONNUMBERSTRING = "08.01.0457.f";
 
     void Awake()
     {
