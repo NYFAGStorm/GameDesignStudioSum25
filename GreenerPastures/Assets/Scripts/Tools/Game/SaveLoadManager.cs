f83f0e7549d4bd87c144637d7ed7d078ffd52c82
Glenn Storm <Glenn.Storm@nyfa.edu>
2025-08-11 15:41:22 -0700

    glenn - save load manager script revised for version number increment.

--- GreenerPastures/Assets/Scripts/Tools/Game/SaveLoadManager.cs
+++ GreenerPastures/Assets/Scripts/Tools/Game/SaveLoadManager.cs
@@ -39,7 +39,7 @@ public class SaveLoadManager : MonoBehaviour
     const string GAMESPATH = "/Games/";
     const string GAMEFILEPREFIX = "GreenerGame-";
     const string GAMEFILESUFFIX = ".dat";
-    const string VERSIONNUMBERSTRING = "08.08.0568.f";
+    const string VERSIONNUMBERSTRING = "08.11.0605.f";
 
     void Awake()
     {
