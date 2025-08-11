a8a1cdba676249047cb4f90d08f90a702c8b5fac
Glenn Storm <Glenn.Storm@nyfa.edu>
2025-08-11 07:28:07 -0700

    glenn - #11, rare clickable added to splash scene, discoverable element script revised to reward player for finding rare clickable element.

--- GreenerPastures/Assets/Scripts/Tools/Menu/DiscoverableElement.cs
+++ GreenerPastures/Assets/Scripts/Tools/Menu/DiscoverableElement.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DiscoverableElement : MonoBehaviour
 {
     // Author: Glenn Storm
     // This handles a static screen clickable element
-
-    // REVIEW: how gamepad can discover element without point and click?
 
     public enum RevealTransition
     {
@@ -22,7 +21,16 @@ public class DiscoverableElement : MonoBehaviour
         Gold,
         XP,
         Arcana,
-        Item
+        MagicItem
+    }
+
+    public enum DiscoverableState
+    {
+        Default,
+        Ready,
+        Appear,
+        Discovered,
+        Rewarded
     }
 
     public Texture2D elementTexture;
@@ -36,16 +44,26 @@ public class DiscoverableElement : MonoBehaviour
     public Vector2 elementTarget;
     public RewardType reward;
     public int rewardAmount;
-    [Tooltip("If reward type is 'Item', define the item type here.")]
-    public ItemType rewardItemType;
 
     private bool elementDiscovered;
     private float revealTimer;
     private float revealProgress;
-    // TODO: set as client player once logged in
-    // REVIEW: hold onto rewards and deliver to next logged in player?
-    // NOTE: consider each of these properties in this player data element an addition
-    private PlayerData playerData = new PlayerData();
+
+    private GreenerGameManager ggm;
+    private PlayerData playerData;
+    private float playerCheckTimer;
+    private float rewardTimer;
+
+    private DiscoverableState state;
+
+    private RewardType storedType;
+    private int storedAmount;
+
+    private AudioManager sfxAudio;
+
+    const float CHANCEOFAPPEARANCE = 0.01f; // big rewards mean rare chance of appearance
+    const float PLAYERCHECKTIME = 10f;
+    const float REWARDTIME = 10f;
 
 
     void Start()
@@ -86,16 +104,18 @@ public class DiscoverableElement : MonoBehaviour
             Debug.LogError("--- DiscoverableElement [Start] : " + gameObject.name + " no reward amount defined. aborting.");
             enabled = false;
         }
-        if (reward == RewardType.Item && rewardItemType == ItemType.Default)
-        {
-            Debug.LogError("--- DiscoverableElement [Start] : " + gameObject.name + " reward type 'Item' defined but no reward item type defined. aborting.");
-            enabled = false;
-        }
+        GameObject sfxObject = GameObject.Find("AudioMgr SFX");
+        if (sfxObject != null)
+            sfxAudio = sfxObject.GetComponent<AudioManager>();
         // initialize
         if (enabled)
         {
             if (revealAnimation.keys == null || revealAnimation.keys.Length == 0)
                 revealAnimation = AnimationCurve.Linear(0f,0f,1f,1f);
+
+            state = DiscoverableState.Default;
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
     }
 
@@ -104,6 +124,83 @@ public class DiscoverableElement : MonoBehaviour
         if (!elementDiscovered)
             return;
 
+        // run player check timer
+        if (playerCheckTimer > 0f)
+        {
+            playerCheckTimer -= Time.deltaTime;
+            if (playerCheckTimer < 0f)
+            {
+                playerCheckTimer = PLAYERCHECKTIME;
+                ggm = GameObject.FindFirstObjectByType<GreenerGameManager>();
+                if (ggm != null)
+                {
+                    // local player gets reward
+                    if (ggm.game != null && ggm.game.players != null && ggm.game.players.Length > 0)
+                    {
+                        playerCheckTimer = 0f;
+                        playerData = ggm.game.players[0];
+                        rewardTimer = REWARDTIME;
+                    }
+                }
+            }
+        }
+
+        // run reward timer
+        if (rewardTimer > 0f)
+        {
+            rewardTimer -= Time.deltaTime;
+            if (rewardTimer < 0f)
+            {
+                rewardTimer = REWARDTIME;
+                // check game manager still up
+                if (ggm == null)
+                {
+                    playerCheckTimer = PLAYERCHECKTIME;
+                    playerData = null;
+                    rewardTimer = 0f;
+                }
+                else
+                {
+                    // check player intro not running
+                    PlayerIntroduction pIntro = GameObject.FindFirstObjectByType<PlayerIntroduction>();
+                    if (pIntro != null)
+                    {
+                        if (!pIntro.introRunning)
+                        {
+                            ProvideReward();
+                            string[] discoverNotifs = new string[2];
+                            discoverNotifs[0] = "You are rewarded for discovering a clickable element!";
+                            switch (reward)
+                            {
+                                case RewardType.Gold:
+                                    discoverNotifs[1] = "You are gifted\n" + rewardAmount + " GOLD!";
+                                    break;
+                                case RewardType.XP:
+                                    discoverNotifs[1] = "You are gifted\n" + rewardAmount + " additional XP!";
+                                    break;
+                                case RewardType.Arcana:
+                                    discoverNotifs[1] = "You are gifted\n" + rewardAmount + " ARCANA!";
+                                    break;
+                                case RewardType.MagicItem:
+                                    discoverNotifs[1] = "You are gifted\n" + rewardAmount + " magic scroll!";
+                                    if (rewardAmount > 1)
+                                        discoverNotifs[1] = "You are gifted\n" + rewardAmount + " magic scrolls!";
+                                    break;
+                            }
+                            ggm.StackNotifications(discoverNotifs);
+                            if (sfxAudio != null)
+                                sfxAudio.StartSound("Player Arcana Skill");
+                            rewardTimer = 0f;
+                            ggm = null;
+                            playerData = null;
+                            elementDiscovered = false;
+                            state = DiscoverableState.Rewarded;
+                        }
+                    }
+                }
+            }
+        }
+
         // run reveal timer
         if (revealTimer > 0f)
         {
@@ -111,12 +208,31 @@ public class DiscoverableElement : MonoBehaviour
             if (revealTimer < 0f)
                 revealTimer = 0f;
             revealProgress = Mathf.Clamp01(1f - (revealTimer/revealTime));
-
             revealProgress = revealAnimation.Evaluate(revealProgress);
-
             if (revealProgress == 1f)
-                ProvideReward();
+            {
+                StoreReward();
+                state = DiscoverableState.Discovered;
+            }
         }
+    }
+
+    void OnSceneLoaded( Scene scene, LoadSceneMode mode )
+    {
+        // activate on credits scene
+        if (scene.name == "Credits" && state == DiscoverableState.Default)
+        {
+            state = DiscoverableState.Ready;
+            // chance of element appearance
+            if ( RandomSystem.FlatRandom01() < CHANCEOFAPPEARANCE )
+                state = DiscoverableState.Appear;
+        }
+        // reward in greenergame scene
+        if (scene.name == "GreenerGame" && state == DiscoverableState.Discovered)
+            playerCheckTimer = PLAYERCHECKTIME;
+        // reset on splash scene
+        if (scene.name == "Splash")
+            state = DiscoverableState.Default;
     }
 
     void RevealElement()
@@ -127,35 +243,84 @@ public class DiscoverableElement : MonoBehaviour
         elementDiscovered = true;
     }
 
+    void StoreReward()
+    {
+        // REVIEW: store indefinitely?
+        storedType = reward;
+        storedAmount = rewardAmount;
+
+        playerCheckTimer = PLAYERCHECKTIME;
+    }
+
     void ProvideReward()
     {
-        switch (reward)
+        if (playerData == null)
+        {
+            Debug.LogWarning("--- DiscoverableElement [ProvideReward] : player data not found on this tool. will ignore.");
+            return;
+        }
+
+        PlayerControlManager pcm = GameObject.FindFirstObjectByType<PlayerControlManager>();
+        if (pcm != null)
+            pcm.AwardXP(PlayerData.XP_FINDCLICKABLE);
+
+        switch (storedType)
         {
             case RewardType.Default:
                 // we should never be here
                 break;
             case RewardType.Gold:
-                playerData.gold += rewardAmount;
+                playerData.gold += storedAmount;
                 break;
             case RewardType.XP:
-                playerData.xp += rewardAmount;
-                // PlayerControlManager.AwardXP( PlayerData.XP_FINDCLICKABLE );
+                playerData.xp += storedAmount;
                 break;
             case RewardType.Arcana:
-                playerData.arcana += rewardAmount;
+                playerData.arcana += storedAmount;
                 break;
-            case RewardType.Item:
-                // TODO: provide item as either inventory (if open slot) or loose item
+            case RewardType.MagicItem:
+                // unknown spell scroll (weighted random)
+                ItemData scroll = InventorySystem.InitializeItem(ItemType.Scroll);
+                scroll.name += " (Unknown)";
+                scroll.effects = new ItemEffect[1];
+                scroll.effects[0] = ItemEffect.ScrollRandomSpellCharge;
+                // provide item as either inventory (if open slot) or loose item
+                for (int i = 0; i < storedAmount; i++)
+                {
+                    if (InventorySystem.InvHasSlot(playerData.inventory))
+                        InventorySystem.AddToInventory(playerData.inventory, scroll);
+                    else
+                    {
+                        LooseItemData looseScroll = InventorySystem.CreateItem(ItemType.Scroll);
+                        looseScroll.inv.items[0] = scroll;
+                        ItemSpawnManager ism = GameObject.FindFirstObjectByType<ItemSpawnManager>();
+                        if (ism != null)
+                        {
+                            Vector3 spawnSpot = Vector3.zero;
+                            Vector3 targetSpot = Vector3.zero;
+                            // if pcm available, spawn at player, if not ... ? at player island?
+                            if (pcm != null)
+                                spawnSpot = pcm.gameObject.transform.position;
+                            else
+                                spawnSpot = GameSystem.GetVector(ggm.game.islands[playerData.playerIsland].location);
+                            targetSpot = spawnSpot + (Vector3.right * 1f * RandomSystem.GaussianRandom01()) - (Vector3.left * 0.5f);
+                            ism.SpawnItem(looseScroll, spawnSpot, targetSpot, true);
+                        }
+                    }
+                }
                 break;
             default:
                 Debug.LogWarning("--- DiscoverableElement [ProvideReward] : " + gameObject.name + " reward type undefined. will ignore.");
                 break;
         }
+
+        storedType = RewardType.Default;
+        storedAmount = 0;
     }
 
     void OnGUI()
     {
-        if (elementDiscovered && revealProgress == 1f)
+        if (state < DiscoverableState.Appear || ( elementDiscovered && revealProgress == 1f ))
             return;
 
         Rect r = elementSpace;
@@ -174,20 +339,57 @@ public class DiscoverableElement : MonoBehaviour
             r.x *= w;
             r.y *= h;
             r.width *= w;
-            r.height *= h;
+            r.height = r.width; // square
 
             GUI.DrawTexture(r, t);
-            // REVIEW: clickable area smaller than image space?
+
             if (GUI.Button(r, "", g))
                 RevealElement();
+
             return;
         }
+
+        // reward label display under element
+        r.x *= w;
+        r.y *= h;
+        r.width *= w;
+        r.height = r.width; // square
+        g = new GUIStyle(GUI.skin.label);
+        g.fontSize = Mathf.RoundToInt( 14f * (w/1024f) );
+        g.fontStyle = FontStyle.Bold;
+        g.alignment = TextAnchor.MiddleCenter;
+        string s = "+" + rewardAmount + "\n";
+        if (reward == RewardType.Gold)
+            s += "GOLD!";
+        else if (reward == RewardType.XP)
+            s += "XP!";
+        else if (reward == RewardType.Arcana)
+            s += "ARCANA!";
+        else
+            s += "MAGIC ITEM!";
+        // drop shadow (yellow over black)
+        r.x += 0.0008f;
+        r.y += 0.001f;
+        g.normal.textColor = Color.black;
+        g.hover.textColor = Color.black;
+        g.active.textColor = Color.black;
+        GUI.Label(r, s, g);
+        r.x -= 0.0016f;
+        r.y -= 0.002f;
+        g.normal.textColor = Color.yellow;
+        g.hover.textColor = Color.yellow;
+        g.active.textColor = Color.yellow;
+        GUI.Label(r, s, g);
+
+        // 
+        r = elementSpace;
+        c = Color.white;
 
         // adjust for lerp to target
         r.x = Mathf.Lerp(elementSpace.x, elementTarget.x, revealProgress);
         r.y = Mathf.Lerp(elementSpace.y, elementTarget.y, revealProgress);
 
-        // REVIEW: consider calculating aspect ratio
+        // 
         Vector2 elementCenter = Vector2.zero;
         elementCenter.x = elementSpace.x + (elementSpace.width * 0.5f);
         elementCenter.y = elementSpace.y + (elementSpace.height * 0.5f);
@@ -224,7 +426,7 @@ public class DiscoverableElement : MonoBehaviour
         r.x *= w;
         r.y *= h;
         r.width *= w;
-        r.height *= h;
+        r.height = r.width; // square
 
         GUI.DrawTexture(r, t);
     }
